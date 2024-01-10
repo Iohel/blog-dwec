7db5643fc05502ebffc20a3afbd85e45c9f91b5d
Iohel <joelpons9@hotmail.com>
2024-01-10 21:12:03 +0100

    DAy 3

---
+++ src/BlogList.js
@@ -0,0 +1,23 @@
+
+const BlogList = ({blogs,titulo, handleEliminarBlog}) => {
+
+    /* const blogs = props.blogs;
+    const titulo = props.titulo; */
+    
+
+
+    return ( 
+        <div className="blog-list">
+            <h2>{titulo}</h2>
+            {blogs.map((blog)=>
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.titulo}</h2>
+                    <p>Escrito por {blog.autor}.</p>
+                    <button onClick={()=>handleEliminarBlog(blog.id)}>Eliminar</button>
+                </div>
+            )}
+        </div>
+    );
+}
+ 
+export default BlogList;
--- src/Inicio.js
+++ src/Inicio.js
@@ -1,24 +1,36 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Inicio = () => {
 
     const[blogs,SetBlogs] = useState([
-        { titulo: "Mi nueva web", body: "Texto para mostrar", autor:"dios", id:1},
+        { titulo: "Mi nueva web", body: "Texto para mostrar", autor:"Dios", id:1},
         { titulo: "Pepe", body: "Pepe", autor:"Pepe", id:2},
-        { titulo: "Matias el Rumano", body: "Encarcelado", autor:"Saitam", id:2}
+        { titulo: "Matias el Rumano", body: "Encarcelado", autor:"Saitam", id:3}
     ])
+
+    const[nombre,setNombre] = useState('Pepito');
+    const[boolean,setBoolean] = useState(false);
+    const handleEliminarBlog = (id)=>{
+        const nuevoBlogs = blogs.filter(blog => blog.id !== id);
+        SetBlogs(nuevoBlogs);
+        setBoolean(true);
+    }
+
+    useEffect(()=>{
+        console.log("GUN");
+        console.log(blogs);
+
+    },[boolean])
 
 	return (
 		<div className="home">
-		    {blogs.map((blog)=>
-                
-                <div className="blog-preview" key={blog.id}>
-                <h2>{blog.titulo}</h2>
-                <p>Escrito por {blog.autor}.</p>
-                </div>
-                
-            )}
-		</div>
+		    <BlogList blogs={blogs} titulo="Listado de blogs" handleEliminarBlog={handleEliminarBlog} />
+		    <BlogList blogs={blogs.filter(blog => blog.autor === "Dios")} titulo="Blogs de Dios" handleEliminarBlog={handleEliminarBlog}/>
+		    <BlogList blogs={blogs.filter(blog => blog.id === 2)} titulo="Blogs de Pepe" handleEliminarBlog={handleEliminarBlog}/>
+        <p>{nombre}</p>
+        <button onClick={()=>setNombre("Pepe")}>Cambio de nombre</button>
+        </div>
 	);
 }
 
